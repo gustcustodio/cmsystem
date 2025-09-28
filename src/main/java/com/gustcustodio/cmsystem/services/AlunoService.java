2d80b555313c14a54039bd7eb8822625f77472a0
gustcustodio <gustcustodio@gmail.com>
2025-09-28 11:48:26 -0300

    feat: add findAll no AlunoService

--- src/main/java/com/gustcustodio/cmsystem/services/AlunoService.java
+++ src/main/java/com/gustcustodio/cmsystem/services/AlunoService.java
@@ -4,10 +4,10 @@ import com.gustcustodio.cmsystem.dtos.AlunoDTO;
 import com.gustcustodio.cmsystem.entities.Aluno;
 import com.gustcustodio.cmsystem.repositories.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 public class AlunoService {
@@ -19,6 +19,12 @@ public class AlunoService {
     public AlunoDTO findById(Long id) {
         Aluno aluno = alunoRepository.findById(id).orElseThrow();
         return new AlunoDTO(aluno);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<AlunoDTO> findAll(Pageable pageable) {
+        Page<Aluno> alunoPage = alunoRepository.findAll(pageable);
+        return alunoPage.map(AlunoDTO::new);
     }
 
 }
