0c2f64cbaa512b4ec9d654b3b5fbf7ec722fa551
gustcustodio <gustcustodio@gmail.com>
2025-09-28 09:34:23 -0300

    feat: entidade Curso

---
+++ src/main/java/com/gustcustodio/cmsystem/entities/Curso.java
@@ -0,0 +1,57 @@
+package com.gustcustodio.cmsystem.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_curso")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+
+    @OneToMany(mappedBy = "curso")
+    private List<Matricula> matriculas = new ArrayList<>();
+
+    public Curso() {
+    }
+
+    public Curso(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
