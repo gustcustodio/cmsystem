f7c8c22ed2c5076020f193e384c052533106036f
gustcustodio <gustcustodio@gmail.com>
2025-09-28 11:47:56 -0300

    feat: add findAll no AlunoController

--- src/main/java/com/gustcustodio/cmsystem/controllers/AlunoController.java
+++ src/main/java/com/gustcustodio/cmsystem/controllers/AlunoController.java
@@ -3,11 +3,10 @@ package com.gustcustodio.cmsystem.controllers;
 import com.gustcustodio.cmsystem.dtos.AlunoDTO;
 import com.gustcustodio.cmsystem.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/alunos")
@@ -20,6 +19,12 @@ public class AlunoController {
     public ResponseEntity<AlunoDTO> findById(@PathVariable Long id) {
         AlunoDTO alunoDTO = alunoService.findById(id);
         return ResponseEntity.ok(alunoDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AlunoDTO>> findAll(Pageable pageable) {
+        Page<AlunoDTO> alunoDTOS = alunoService.findAll(pageable);
+        return ResponseEntity.ok(alunoDTOS);
     }
 
 }
