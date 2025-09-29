bf6b01d9a1247009d1f83239ba65be215b875d0d
gustcustodio <gustcustodio@gmail.com>
2025-09-29 06:52:23 -0300

    feat: add findAll no CourseService

--- src/main/java/com/gustcustodio/cmsystem/services/CourseService.java
+++ src/main/java/com/gustcustodio/cmsystem/services/CourseService.java
@@ -4,6 +4,8 @@ import com.gustcustodio.cmsystem.dtos.CourseDTO;
 import com.gustcustodio.cmsystem.entities.Course;
 import com.gustcustodio.cmsystem.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,12 @@ public class CourseService {
     public CourseDTO findById(Long id) {
         Course course = courseRepository.findById(id).orElseThrow();
         return new CourseDTO(course);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CourseDTO> findAll(Pageable pageable) {
+        Page<Course> coursePage = courseRepository.findAll(pageable);
+        return coursePage.map(CourseDTO::new);
     }
 
 }
