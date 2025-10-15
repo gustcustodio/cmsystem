3b792ee3ca01ef7781a1f2089878ea3cf7daf9a0
gustcustodio <gustcustodio@gmail.com>
2025-10-15 06:30:07 -0300

    feat: add equals e hashCode na classe StudentCoursePK

--- src/main/java/com/gustcustodio/cmsystem/entities/StudentCoursePK.java
+++ src/main/java/com/gustcustodio/cmsystem/entities/StudentCoursePK.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class StudentCoursePK {
 
@@ -32,6 +34,21 @@ public class StudentCoursePK {
 
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StudentCoursePK that = (StudentCoursePK) o;
+        return Objects.equals(student, that.student) && Objects.equals(course, that.course);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(student);
+        result = 31 * result + Objects.hashCode(course);
+        return result;
     }
 
 }
